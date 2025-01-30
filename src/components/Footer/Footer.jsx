c18a7cdb77b2a16a20a8b7f67dda36c18de9501a
Michael P <102651473+michmich16@users.noreply.github.com>
2025-01-30 03:00:17 +0100

    jeg giver op

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-export const Footer = () =>{
-    return(
-        <>
-        <footer>
-            Footer here
-        </footer>
-        </>
-    )
+import s from "./Footer.module.scss";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+export const Footer = () => {
+    return (
+        <div className={s.footerContainer}>
+            <p>© 2021 hotel Overlook. Alle rettigheder forbeholdt.</p>
+            <div className={s.footerIcons}>
+                <FaTwitterSquare />
+                <FaFacebook />
+            </div>
+            <ul className={s.footerNav}>
+                <li><NavLink to="/">FORSIDE</NavLink></li>
+                <li><NavLink to="/info">HOTELLER & DESTINATIONER</NavLink></li>
+                <li><NavLink to="/rooms">VÆRELSER</NavLink></li>
+                <li><NavLink to="/reservation">RESERVATION</NavLink></li>
+                <li><NavLink to="/login">LOGIN</NavLink></li>
+            </ul>
+        </div>
+    );
 }
--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -1,8 +1,51 @@
+import { InputField } from '../components/InputField/InputField'
+import { GridContainer } from '../components/GridContainer/GridContainer'
+import { MarginContainer } from '../components/MarginContainer/MarginContainer'
+import { SectionTitle } from '../components/SectionTitle/SectionTitle'
+import { useContext, useState } from 'react';
+import s from '../styles/ReservationPage.module.scss';
+
 export const ReservationPage = () =>{
 
     return(
         <>
-        ReservationPage
+         <GridContainer columns="3fr 1fr">
+                <MarginContainer border="1px solid grey" margin="1rem" height="70vh">
+                    <SectionTitle title={"Reservation"} />
+                    <form className={s.formStyle}>
+                        <InputField
+                            type="text"
+                            placeholder="Fornavn"
+                            name="firstname"
+                            id="firstnameField"
+                            // action={setFirstname}
+                        />
+                        <InputField
+                            type="text"
+                            placeholder="Efternavn"
+                            name="lastname"
+                            id="lastnameField"
+                            // action={setLastname}
+                        />
+                        <InputField
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            id="emailField"
+                            // action={setEmail}
+                        />
+                        <InputField
+                            type="password"
+                            placeholder="Adgangskode"
+                            name="password"
+                            id="passwordField"
+                            // action={setPassword}
+                        />
+                    </form>
+
+                </MarginContainer>
+                <MarginContainer />
+            </GridContainer>
         </>
     )
 }
